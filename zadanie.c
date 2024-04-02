384d2e12272b6f5e1f03d09c22c77dbb9bef91d6
midiachok <162707182+midiachok@users.noreply.github.com>
2024-04-02 16:01:30 +0200

    .

--- zadanie.c
+++ zadanie.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+typedef struct{
+     int n;
+     double b[nn];	
+}mnozina;
+
 int  main(){
+	
+	int nn;
+	int a[n];
+	
+     mnozina.n=scanf("%d",nn);//pocet prvkov
+	for(i=0;i<nn;i++){
+			mnozina.b[i]=scanf("%lf",b[i]);
+		}
+	
 	
 }
