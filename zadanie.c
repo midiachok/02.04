9aff55e4381cc148a91da7d489397b1c01c92215
midiachok <162707182+midiachok@users.noreply.github.com>
2024-04-02 16:18:42 +0200

    .

--- zadanie.c
+++ zadanie.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 typedef struct{
-     int n;
-     double b[nn];	
+     int nn;//pocet prvkov
+     double *b[nn];
 }mnozina;
 
 int  main(){
 	
-	int nn;
-	int a[n];
+	int nn,i;
+	mnozina m;
 	
-     mnozina.n=scanf("%d",nn);//pocet prvkov
+	printf("zadajte pocet prvkov");
+	scanf("%d",&m.nn);
+	 
+	m.b = (double *)malloc(m.nn * sizeof(double));
+	 
+     mnozina.nn=scanf("%d",mnozina.nn);
 	for(i=0;i<nn;i++){
-			mnozina.b[i]=scanf("%lf",b[i]);
+			mnozina.b[i]=scanf("%lf",mnozina.b[i]);
+			printf("%lf",mnozina.b[i]);
 		}
-	
 	
 }
